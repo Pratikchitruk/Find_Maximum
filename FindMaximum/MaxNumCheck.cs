15c0bd47c128804ea2f85a4dc119f514376fa4b9
PratikChitruk <pratik.chitruk@gmail.com>
2022-06-25 12:26:49 +0530

    [pratik]Refactor : Previous int test cases included.

--- FindMaximum/MaxNumCheck.cs
+++ FindMaximum/MaxNumCheck.cs
@@ -16,5 +16,17 @@ namespace FindMaximum
                 return num3;
             return 0;
         }
+
+        public float FloatMax(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+                return num1;
+            if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+                return num2;
+            if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+                return num3;
+            return 0;
+        }
+
     }
 }
